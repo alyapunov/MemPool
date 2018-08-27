d8bf11e1ed993ec10165cbf3d4c8d19442307b5e
Aleksandr Lyapunov <a.lyapunov@corp.mail.ru>
2018-08-27 14:50:29 +0300

    fix and unit tests

--- GlobalMemPool.hpp
+++ GlobalMemPool.hpp
@@ -26,6 +26,8 @@ public:
     static std::mutex& mutex() { return instance().internalMutex(); }
     static char* alloc() { return instance().internalAlloc(); }
     static void free(char* aBlock) { return instance().internalFree(aBlock); }
+    static size_t slabCount() { return instance().m_SlabCount; }
+    static size_t freeCount() { return instance().m_FreeCount; }
 
 private:
     using Block = MemPoolBlock<SIZE>;
@@ -34,7 +36,9 @@ private:
     // By my measurements syscall performance is about 3Mrps.
     // Set BLOCKS_IN_SLAB big enough avoiding making the mmap call a bottleneck.
     static constexpr size_t SYSCALL_DIVIDER = 256;
+public:
     static constexpr size_t SLAB_SIZE = memPoolCeil2(BLOCK_SIZE * SYSCALL_DIVIDER);
+private:
 
     struct Slab;
     struct SlabHeader
@@ -44,14 +48,18 @@ private:
         Block* m_FreeList = nullptr;
         size_t m_FreeCount = 0;
         Slab* m_NextNew = nullptr;
+        bool m_IsInFreeList = false;
         void* operator new(size_t) = delete;
         void operator delete(void*) = delete;
     };
 
     static constexpr size_t PADDING_SIZE = (SLAB_SIZE - sizeof(SlabHeader)) % BLOCK_SIZE;
+public:
     static constexpr size_t BLOCKS_IN_SLAB = (SLAB_SIZE - sizeof(SlabHeader)) / BLOCK_SIZE;
     static_assert(BLOCKS_IN_SLAB > 1, "Too small slab");
-    static constexpr size_t FREE_SHIFT = memPoolLog2(BLOCKS_IN_SLAB) - 1;
+    static constexpr size_t MAX_MMAP_SLABS = 6;
+private:
+    static constexpr size_t MOVE_GROUP_THRESHOLD_PERCENT = 5;
     struct Slab : SlabHeader, GlobalMemPoolPadding<PADDING_SIZE>
     {
         Block m_Blocks[BLOCKS_IN_SLAB];
@@ -83,7 +91,7 @@ private:
     {
         Slab* sSlab = new (aMem) Slab;
         m_AllList.insertBack(*sSlab);
-        m_SlabCount++;
+        ++m_SlabCount;
         return sSlab;
     }
 
@@ -105,13 +113,17 @@ private:
         SlabHeader* sSlab = &m_FreeList[m_FreeList[0].empty()].front();
         char* sRes = sSlab->m_FreeList->m_Data;
         sSlab->m_FreeList = sSlab->m_FreeList->m_Next;
-        size_t sWasFreeCount = sSlab->m_FreeCount--;
+        --sSlab->m_FreeCount;
+        const size_t MOVE_GROUP_TRESHOLD = BLOCKS_IN_SLAB * (50 - MOVE_GROUP_THRESHOLD_PERCENT) / 100;
         if (MEMPOOL_UNLIKELY(0 == sSlab->m_FreeCount))
         {
+            assert(!m_FreeList[0].empty());
             m_FreeList[0].remove(*sSlab);
+            sSlab->m_IsInFreeList = false;
         }
-        else if (MEMPOOL_UNLIKELY((sWasFreeCount ^ sSlab->m_FreeCount) >> FREE_SHIFT))
+        else if (MOVE_GROUP_TRESHOLD == sSlab->m_FreeCount)
         {
+            assert(m_FreeList[0].empty());
             m_FreeList[1].remove(*sSlab);
             m_FreeList[0].insert(*sSlab);
         }
@@ -120,7 +132,6 @@ private:
 
     char* mmapAndAlloc()
     {
-        const size_t MAX_COUNT = 4;
         size_t sCount = 1;
         char* sPtr = mmap(SLAB_SIZE);
         uintptr_t sAddr = reinterpret_cast<uintptr_t>(sPtr);
@@ -128,10 +139,11 @@ private:
         {
             if (MEMPOOL_LIKELY(MAP_FAILED != sPtr))
                 munmap(sPtr, SLAB_SIZE);
-            sPtr = mmap(MAX_COUNT * SLAB_SIZE);
+            sPtr = mmap(MAX_MMAP_SLABS * SLAB_SIZE);
+            sAddr = reinterpret_cast<uintptr_t>(sPtr);
             if (MEMPOOL_UNLIKELY(MAP_FAILED == sPtr))
                 throw std::bad_alloc();
-            sCount = MAX_COUNT;
+            sCount = MAX_MMAP_SLABS;
             if (MEMPOOL_LIKELY(0 != sAddr % SLAB_SIZE))
             {
                 --sCount;
@@ -158,11 +170,14 @@ private:
         sBlock->m_Next = sSlab->m_FreeList;
         sSlab->m_FreeList = sBlock;
         size_t sWasFreeCount = sSlab->m_FreeCount++;
-        if (MEMPOOL_UNLIKELY(0 == sWasFreeCount))
+        const size_t INTRODUCE_GROUP_TRESHOLD = BLOCKS_IN_SLAB * MOVE_GROUP_THRESHOLD_PERCENT / 100;
+        const size_t MOVE_GROUP_TRESHOLD = BLOCKS_IN_SLAB * (50 + MOVE_GROUP_THRESHOLD_PERCENT) / 100;
+        if (MEMPOOL_UNLIKELY(INTRODUCE_GROUP_TRESHOLD == sWasFreeCount && !sSlab->m_IsInFreeList))
         {
             m_FreeList[0].insert(*sSlab);
+            sSlab->m_IsInFreeList = true;
         }
-        else if (MEMPOOL_UNLIKELY((sWasFreeCount ^ sSlab->m_FreeCount) >> FREE_SHIFT))
+        else if (MEMPOOL_UNLIKELY(MOVE_GROUP_TRESHOLD == sWasFreeCount))
         {
             m_FreeList[0].remove(*sSlab);
             m_FreeList[1].insert(*sSlab);
@@ -179,13 +194,16 @@ private:
     Slab* slabByBlock(char* aBlock)
     {
         uintptr_t sAddr = reinterpret_cast<uintptr_t>(aBlock);
-        sAddr &= ~(SLAB_SIZE - 1);
+        uintptr_t sLowMask = SLAB_SIZE - 1;
+        sAddr &= ~sLowMask;
         return reinterpret_cast<Slab*>(sAddr);
     }
 
     std::mutex m_Mutex;
     AutoList<SlabHeader, &SlabHeader::m_AllLink> m_AllList;
-    // 0 - less than half free (but not 100% full), 1 - more than half full.
+    // 0 - more than half full (but not 100% full), 1 - less than half full .
+    // e.g. 0 : m_FreeCount <= BLOCKS_IN_SLAB / 2; 1 : m_FreeCount > BLOCKS_IN_SLAB / 2.
+    // Actually that boundaries are approximate, the move from one group to another may be delayed.
     SlightlyOrderedList<SlabHeader, &SlabHeader::m_FreeLink, SLAB_SIZE> m_FreeList[2];
     size_t m_SlabCount = 0;
     size_t m_FreeCount = 0;
--- GlobalMemPoolUnitTest.cpp
+++ GlobalMemPoolUnitTest.cpp
@@ -1,9 +1,55 @@
 #include <GlobalMemPool.hpp>
 
+#include <iostream>
+#include <unordered_map>
+#include <vector>
 
-int main()
+int rc = 0;
+
+void check(bool exp, const char* funcname, const char *filename, int line)
 {
+    if (!exp)
+    {
+        rc = 1;
+        std::cerr << "Check failed in " << funcname << " at " << filename << ":" << line << std::endl;
+    }
+}
 
+template<class T>
+void check(const T& x, const T& y, const char* funcname, const char *filename, int line)
+{
+    if (x != y)
+    {
+        rc = 1;
+        std::cerr << "Check failed: " << x << " != " << y <<  " in " << funcname << " at " << filename << ":" << line << std::endl;
+    }
+}
+
+
+#define CHECK(...) check(__VA_ARGS__, __func__, __FILE__, __LINE__)
+
+struct Announcer
+{
+    const char* m_Func;
+    explicit Announcer(const char* aFunc) : m_Func(aFunc) { std::cout << "Test " << m_Func << " started" << std::endl; }
+    ~Announcer() { std::cout << "Test " << m_Func << " finished" << std::endl; }
+};
+
+#define ANNOUNCE() Announcer sAnn(__func__)
+
+struct AnnouncerTmpl
+{
+    const char* m_Func;
+    size_t m_Tmpl;
+    explicit AnnouncerTmpl(const char* aFunc, size_t aTmpl) : m_Func(aFunc), m_Tmpl(aTmpl) { std::cout << "Test " << m_Func << "<" << m_Tmpl << "> started" << std::endl; }
+    ~AnnouncerTmpl() { std::cout << "Test " << m_Func << "<" << m_Tmpl << "> finished" << std::endl; }
+};
+
+#define ANNOUNCE_TMPL(N) AnnouncerTmpl sAnn(__func__, N)
+
+static void compile_test()
+{
+    ANNOUNCE();
     GlobalMemPool<1>::alloc();
     GlobalMemPool<2>::alloc();
     GlobalMemPool<3>::alloc();
@@ -63,4 +109,174 @@ int main()
     GlobalMemPool<57>::alloc();
     GlobalMemPool<58>::alloc();
     GlobalMemPool<59>::alloc();
+    GlobalMemPool<60>::alloc();
+    GlobalMemPool<61>::alloc();
+    GlobalMemPool<62>::alloc();
+    GlobalMemPool<63>::alloc();
+    GlobalMemPool<64>::alloc();
+    GlobalMemPool<65>::alloc();
+    GlobalMemPool<66>::alloc();
+    GlobalMemPool<67>::alloc();
+    GlobalMemPool<68>::alloc();
+    GlobalMemPool<69>::alloc();
+    GlobalMemPool<70>::alloc();
+}
+
+template <size_t S>
+struct Thing
+{
+    unsigned char m_Arr[S];
+    static_assert(S >= sizeof(size_t), "Size is too small");
+    void set(size_t aNum)
+    {
+        for (size_t i = 0; i < sizeof(size_t); i++)
+        {
+            m_Arr[i] = static_cast<unsigned char>(aNum);
+            aNum >>= 8;
+        }
+        for (size_t i = sizeof(size_t); i < S; i++)
+        {
+            m_Arr[i] = m_Arr[i - sizeof(size_t)] + static_cast<unsigned char>(1);
+        }
+    }
+    bool good(size_t aNum) const
+    {
+        for (size_t i = 0; i < sizeof(size_t); i++)
+        {
+            if (m_Arr[i] != static_cast<unsigned char>(aNum))
+                return false;
+            aNum >>= 8;
+        }
+        for (size_t i = sizeof(size_t); i < S; i++)
+        {
+            unsigned char sCmp = m_Arr[i - sizeof(size_t)] + static_cast<unsigned char>(1);
+            if (m_Arr[i] != sCmp)
+                return false;
+        }
+        return true;
+    }
+    Thing(size_t aNum) { set(aNum); }
+    void* operator new(size_t)
+    {
+        return GlobalMemPool<sizeof(Thing)>::alloc();
+    }
+    void operator delete(void* aPtr)
+    {
+        GlobalMemPool<sizeof(Thing)>::free(static_cast<char*>(aPtr));
+    }
+};
+
+template <size_t S>
+static void seq_test()
+{
+    ANNOUNCE_TMPL(S);
+    size_t sSlabCountOrig = GlobalMemPool<S>::slabCount();
+    const size_t N = 8 * 1024;
+    std::unordered_map<size_t, Thing<S>*> sMap;
+    for (size_t i = 0; i < N; i++)
+    {
+        for (const auto& sPair : sMap)
+            CHECK(sPair.second->good(sPair.first));
+        sMap[i] = new Thing<S>(i);
+        CHECK(GlobalMemPool<S>::slabCount() > sSlabCountOrig);
+    }
+    for (size_t i = 0; i < N; i++)
+    {
+        delete sMap[i];
+        sMap.erase(i);
+        for (const auto& sPair : sMap)
+            CHECK(sPair.second->good(sPair.first));
+        CHECK(GlobalMemPool<S>::slabCount() > sSlabCountOrig);
+    }
+    CHECK(GlobalMemPool<S>::slabCount() <= GlobalMemPool<S>::MAX_MMAP_SLABS + 1);
+}
+
+template <size_t S>
+static void seq_rev_test()
+{
+    ANNOUNCE_TMPL(S);
+    size_t sSlabCountOrig = GlobalMemPool<S>::slabCount();
+    const size_t N = 8 * 1024;
+    std::unordered_map<size_t, Thing<S>*> sMap;
+    for (size_t i = 0; i < N; i++)
+    {
+        for (const auto& sPair : sMap)
+            CHECK(sPair.second->good(sPair.first));
+        sMap[i] = new Thing<S>(i);
+        CHECK(GlobalMemPool<S>::slabCount() > sSlabCountOrig);
+    }
+    for (size_t k = 0; k < N; k++)
+    {
+        size_t i = N - 1 - k;
+        delete sMap[i];
+        sMap.erase(i);
+        for (const auto& sPair : sMap)
+            CHECK(sPair.second->good(sPair.first));
+        CHECK(GlobalMemPool<S>::slabCount() > sSlabCountOrig);
+    }
+    CHECK(GlobalMemPool<S>::slabCount() <= GlobalMemPool<S>::MAX_MMAP_SLABS + 1);
+}
+
+template <size_t S>
+static void rand_test()
+{
+    ANNOUNCE_TMPL(S);
+    size_t sSlabCountOrig = GlobalMemPool<S>::slabCount();
+    const size_t N = 8 * 1024;
+    const size_t M = N / 16;
+    std::unordered_map<size_t, Thing<S>*> sMap;
+    for (size_t i = 0; i < N; i++)
+    {
+        sMap[i] = new Thing<S>(i);
+        CHECK(GlobalMemPool<S>::slabCount() > sSlabCountOrig);
+    }
+
+    for (size_t i = 0; i < N; i++)
+    {
+        int c = rand() % M;
+        std::vector<size_t> xs;
+        xs.reserve(c);
+        for (int j = 0; j < c; j++)
+        {
+            size_t x = rand() % M;
+            if (sMap.find(x) != sMap.end())
+            {
+                delete sMap[x];
+                sMap.erase(x);
+                xs.push_back(x);
+            }
+        }
+        for (const auto& sPair : sMap)
+            CHECK(sPair.second->good(sPair.first));
+        for (size_t x : xs)
+            sMap[x] = new Thing<S>(x);
+        for (const auto& sPair : sMap)
+            CHECK(sPair.second->good(sPair.first));
+    }
+
+    for (size_t i = 0; i < N; i++)
+    {
+        delete sMap[i];
+        sMap.erase(i);
+        for (const auto& sPair : sMap)
+            CHECK(sPair.second->good(sPair.first));
+        CHECK(GlobalMemPool<S>::slabCount() > sSlabCountOrig);
+    }
+    CHECK(GlobalMemPool<S>::slabCount() <= GlobalMemPool<S>::MAX_MMAP_SLABS + 1);
+}
+
+
+int main()
+{
+    if (time(nullptr) == 0)
+        compile_test();
+
+    seq_test<64>();
+    seq_rev_test<65>();
+    rand_test<66>();
+    seq_test<200>();
+    seq_rev_test<201>();
+    rand_test<202>();
+
+    return rc;
 }
